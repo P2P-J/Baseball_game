97abf045225f45bdde2fad7ae31105c9c4d7222c
KimJeongChul <jhtpo1230@skuniv.ac.kr>
2025-09-16 15:20:25 +0900

    comRandNum get - BG

--- baseballGame.js
+++ baseballGame.js
@@ -1,1 +1,18 @@
-test
+let comNUm = [];
+let isGameRunning = false;
+
+// 컴퓨터가 뽑는 임의의 1~9 랜덤 숫자
+function randomNum() {
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let comNumbers = [];
+
+    for (let i=0; i<3; i++) {
+        let getPosition = Math.floor(Math.random() * numbers.length);
+        let getNumber = numbers[getPosition];
+
+        comNumbers.push(getNumber);
+        numbers.splice(getPosition, 1);
+    }
+    console.log("컴퓨터가 숫자를 뽑았습니다." );
+    return comNumbers;
+}
