bb7993893aa17814ccf2da01755e34f04339b164
KimJeongChul <jhtpo1230@skuniv.ac.kr>
2025-09-17 01:28:55 +0900

    userInput function commit - BG

--- baseballGame.js
+++ baseballGame.js
@@ -1,34 +1,62 @@
 // let comNum = [];
 let isGameRunning = false;
-
 const readline = require("readline");
-
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+    input: process.stdin,
+    output: process.stdout,
 });
 
 // 컴퓨터가 뽑는 임의의 1~9 랜덤 숫자 함수입니다
 function randomNum() {
-  let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  let comNumbers = [];
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let comNumbers = [];
 
-  for (let i = 0; i < 3; i++) {
-    let getPosition = Math.floor(Math.random() * numbers.length);
-    let getNumber = numbers[getPosition];
+    for (let i = 0; i < 3; i++) {
+        let getPosition = Math.floor(Math.random() * numbers.length);
+        let getNumber = numbers[getPosition];
 
-    comNumbers.push(getNumber);
-    numbers.splice(getPosition, 1);
-  }
-  console.log("컴퓨터가 숫자를 뽑았습니다.");
-  return comNumbers;
+        comNumbers.push(getNumber);
+        numbers.splice(getPosition, 1);
+    }
+    console.log("\n컴퓨터가 숫자를 뽑았습니다.");
+    return comNumbers;
 }
 
+// 게임 시작 함수
+function gameStart() {
+    console.log("게임을 새로 시작하려면 1, 종료하려면 9를 입력하세요.");
+    rl.question("", (answer) => {
+        if (Number(answer) == 1) {
+            randomNum();
+            compareScore(randomNum(), userinput()); //compareScore : 판정함수
+
+        } else if (Number(answer) == 9) {
+            console.log("어플리케이션이 종료되었습니다!!");
+            rl.close();
+        } else {
+            gameStart();
+        }
+    });
+}
+
+// 유저 입력값 받는 함수
 function userinput() {
-    
+    rl.question("3자리 숫자 입력 : ", (answer) => {
+        if (answer.length !== 3) {
+            console.log("숫자는 3자리 숫자로 입력해주세요.");
+            userinput();
+        } else {
+            return Number(answer);
+        }
+    });
 }
 
-// function 판정() {
+// 컴퓨터 랜덤숫자, 유저 숫자 비교 함수
+function compareScore(randomNum, userinput) {
+
+}
+
+// function 판정(randomNum, userinput) {
 //     let strike = 0;
 //     let ball = 0;
 
@@ -41,24 +69,5 @@ function userinput() {
 
 // }
 
-function gameStart() {
-  console.log(
-    "게임을 시작하시겠습니까? : 1번을 눌러주세요 || 게임을 종료하시겠습니까? : 9번을 눌러주세요"
-  );
-  rl.question("", (answer) => {
-    if (Number(answer) == 1) {
-      randomNum();
-      userinput();
-      판정();
-    } else if (Number(answer) == 9) {
-      console.log("어플리케이션이 종료되었습니다!!");
-      rl.close();
-    } else {
-      gameStart();
-    }
-  });
-}
+
 gameStart();
-
-
-test push
